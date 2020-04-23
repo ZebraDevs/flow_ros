3820ed33458b89a8926a1212c2437b323ba90cc7
Brian Cairl <bcairl@fetchrobotics.com>
2020-04-23 23:34:24 +0000

    Clear up MessageDispatch specializations
    - Adds Specialization-helper based with extra methods for interfacing with dispatch object

--- flow_ros/include/messages.h
+++ flow_ros/include/messages.h
@@ -60,25 +60,121 @@ using message_shared_const_ptr_t = typename MessageSharedConstPtrType<MsgT>::typ
 
 
 /**
+ * @brief Template which basic methods for MessageDispatch
+ *
+ * @tparam MsgT  message type
+ */
+template<typename MsgT>
+class MessageDispatchSpecializationBase
+{
+public:
+  using message_resource_type = typename MessageSharedPtrType<MsgT>::type;
+
+  MessageDispatchSpecializationBase() = default;
+  MessageDispatchSpecializationBase(const MessageDispatchSpecializationBase&) = default;
+
+  /**
+   * @brief Message constructor (move enabled)
+   */
+  explicit MessageDispatchSpecializationBase(message_resource_type msg) :
+    msg_{std::move(msg)}
+  {}
+
+  /**
+   * @brief Checks if message contents are valid
+   */
+  inline bool valid() const
+  {
+    return static_cast<bool>(msg_);
+  }
+
+  /**
+   * @copydoc MessageDispatchSpecializationBase::valid
+   */
+  inline operator bool() const
+  {
+    return this->valid();
+  }
+
+  /**
+   * @brief Derefences message resource
+   */
+  inline const MsgT& operator*() const
+  {
+    return *msg_;
+  }
+
+  /**
+   * @brief Derefences message resource
+   */
+  inline const MsgT* operator->() const
+  {
+    return msg_.get();
+  }
+
+  /**
+   * @brief Message resource conversion operator overload
+   */
+  inline operator const message_resource_type&() const
+  {
+    return msg_;
+  }
+
+  /**
+   * @brief Returns const reference to underlying data element
+   */
+  inline const typename MessageSharedPtrType<MsgT>::type& data() const
+  {
+    return this->msg_;
+  }
+
+protected:
+  /// Message payload
+  typename MessageSharedPtrType<MsgT>::type msg_;
+};
+
+
+/**
  * @brief Flow dispatch object for ROS messages with a Header field
  *
  * @tparam MsgT  message type
- * @tparam IS_ROS_MESSAGE  [DO NOT SUPPLY THIS ARGUMENT] (defaulted) to see if MsgT is a ROS message
  *
  * @note May be specialized for any message which time stamp info
  */
-template<typename MsgT, bool IS_ROS_MESSAGE = is_message_v<MsgT>>
-class MessageDispatch;
+template<typename MsgT>
+class MessageDispatch : public MessageDispatchSpecializationBase<MsgT>
+{
+public:
+  template<typename ConstructorMsgT>
+  inline MessageDispatch(ConstructorMsgT&& msg) :
+    MessageDispatchSpecializationBase<MsgT>{std::forward<ConstructorMsgT>(msg)}
+  {}
+
+  /**
+   * @brief Returns sequencing stamp associated with data element
+   */
+  inline const ros::Time& stamp() const
+  {
+    return this->msg_->header.stamp;
+  }
+};
 
 
 /**
  * @brief Helper object use to set message stamps
  *
  * @tparam MsgT  message type
- * @tparam IS_ROS_MESSAGE  [DO NOT SUPPLY THIS ARGUMENT] (defaulted) to see if MsgT is a ROS message
+ *
+ * @note May be specialized for any message which time stamp info
  */
-template<typename MsgT, bool IS_ROS_MESSAGE = is_message_v<MsgT>>
-struct StampSetter;
+template<typename MsgT>
+struct StampSetter
+{
+  inline void operator()(MsgT& msg, const ros::Time& stamp) const
+  {
+    msg.header.stamp = stamp;
+  }
+};
 
 
 /**
