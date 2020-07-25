315f9859a18b52b3f3ddd9f635a765b06fe71c00
briancairl <briancairl@gmail.com>
2020-07-24 17:26:31 -0700

    Adds more basic subsciber unit tests

--- flow_ros/test/unit/subscriber.cpp
+++ flow_ros/test/unit/subscriber.cpp
@@ -55,3 +55,125 @@ TEST(SubscriberLocal, FollowingPolicyDefault)
   EXPECT_EQ(sub.size(), 0UL);
   EXPECT_EQ(sub.getCapacity(), 0UL);
 }
+
+
+TEST(SubscriberLocal, MessageInjectSameStamp)
+{
+  flow_ros::Router router{"/router"};
+
+  flow_ros::Subscriber<TestMessage, flow::follower::Before, flow::NoLock> sub{router, "topic", 1, ros::Duration{0}};
+
+  ASSERT_EQ(sub.get_capacity(), 0UL);
+
+  for (int i = 0; i < 10; ++i)
+  {
+    sub.inject([]
+      {
+        auto msg = std::make_shared<TestMessage>();
+        msg->header.stamp.fromSec(1);
+        return msg;
+      }());
+  }
+
+  EXPECT_EQ(sub.size(), 1UL);
+}
+
+
+TEST(SubscriberLocal, MessageInjectSequence)
+{
+  flow_ros::Router router{"/router"};
+
+  flow_ros::Subscriber<TestMessage, flow::follower::Before, flow::NoLock> sub{router, "topic", 1, ros::Duration{0}};
+
+  ASSERT_EQ(sub.get_capacity(), 0UL);
+
+  for (int i = 0; i < 2; ++i)
+  {
+    sub.inject([i]
+      {
+        auto msg = std::make_shared<TestMessage>();
+        msg->header.stamp.fromSec(i);
+        return msg;
+      }());
+  }
+
+  EXPECT_EQ(sub.size(), 2UL);
+}
+
+
+TEST(SubscriberLocal, MessageInjectFixedBufferCapacity)
+{
+  flow_ros::Router router{"/router"};
+
+  flow_ros::Subscriber<TestMessage, flow::follower::Before, flow::NoLock> sub{router, "topic", 1, ros::Duration{0}};
+
+  sub.set_capacity(3UL);
+  ASSERT_EQ(sub.get_capacity(), 3UL);
+
+  for (int i = 0; i < 10; ++i)
+  {
+    sub.inject([i]
+      {
+        auto msg = std::make_shared<TestMessage>();
+        msg->header.stamp.fromSec(i);
+        return msg;
+      }());
+  }
+
+  EXPECT_EQ(sub.size(), 3UL);
+}
+
+
+TEST(SubscriberLocal, MessageRemove)
+{
+  flow_ros::Router router{"/router"};
+
+  flow_ros::Subscriber<TestMessage, flow::follower::Before, flow::NoLock> sub{router, "topic", 1, ros::Duration{0}};
+
+  ASSERT_EQ(sub.get_capacity(), 0UL);
+
+  for (int i = 0; i < 10; ++i)
+  {
+    sub.inject([i]
+      {
+        auto msg = std::make_shared<TestMessage>();
+        msg->header.stamp.fromSec(i);
+        return msg;
+      }());
+  }
+
+  sub.remove(ros::Time{2});
+
+  // Removed all before 2, leaving 8 more
+  EXPECT_EQ(sub.size(), 8UL);
+}
+
+
+TEST(SubscriberLocal, MessageCapture)
+{
+  flow_ros::Router router{"/router"};
+
+  flow_ros::Subscriber<TestMessage, flow::follower::Before, flow::NoLock> sub{router, "topic", 1, ros::Duration{0}};
+
+  ASSERT_EQ(sub.get_capacity(), 0UL);
+
+  sub.inject([]
+    {
+      auto msg = std::make_shared<TestMessage>();
+      msg->header.stamp.fromSec(1);
+      return msg;
+    }());
+
+  sub.inject([]
+    {
+      auto msg = std::make_shared<TestMessage>();
+      msg->header.stamp.fromSec(2);
+      return msg;
+    }());
+
+  std::vector<TestMessage::ConstPtr> messages;
+  sub.capture(std::back_inserter(messages), flow::CaptureRange<ros::Time>{ros::Time{2}, ros::Time{2}});
+
+  EXPECT_EQ(sub.size(), 1UL);
+  EXPECT_EQ(messages.size(), 1UL);
+}
